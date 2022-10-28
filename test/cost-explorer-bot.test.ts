88db3693d317c0ededa5bda7a66bac1a7edad743
haneyume <haneyume@gmail.com>
2022-10-28 17:06:50 +0800

    Initial commit

---
+++ test/cost-explorer-bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CostExplorerBot from '../lib/cost-explorer-bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cost-explorer-bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CostExplorerBot.CostExplorerBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
